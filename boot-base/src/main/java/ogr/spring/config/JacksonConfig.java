5b01e6a65c3cbe2edb52bddedbfd74561b774df4
醉卧意逍遥 <57378028+IloveChina77@users.noreply.github.com>
2020-05-27 22:37:45 +0800

    对jackson序列化中的特殊情况做处理

--- boot-base/src/main/java/ogr/spring/config/JacksonConfig.java
+++ boot-base/src/main/java/ogr/spring/config/JacksonConfig.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class JacksonConfig {
 
-    // Integer [] list
+    // 为null的数据类型
+    // Integer Double String  Array List Map  date
+    // 0       0.0    ""      []    []        13timestamp
 
 
 }
