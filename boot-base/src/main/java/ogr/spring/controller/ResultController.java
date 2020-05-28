07a911793cf5fa6eef38af7b1ab52c45e01c2dd4
周锐 <57378028+IloveChina77@users.noreply.github.com>
2020-05-28 20:05:37 +0800

    修改pom文件

--- boot-base/src/main/java/ogr/spring/controller/ResultController.java
+++ boot-base/src/main/java/ogr/spring/controller/ResultController.java
@@ -1,33 +1,43 @@
 package ogr.spring.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import ogr.spring.pojo.UserVO;
 import ogr.spring.result.Result;
 import ogr.spring.result.ResultUtil;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Api("测试统一返回结果接口")
 @RestController
 @RequestMapping("/resultController")
 public class ResultController {
 
+    @ApiOperation("直接获取result")
     @GetMapping("/getRes")
     public <T> Result<T> getRes() {
         return ResultUtil.success();
     }
 
+    @ApiOperation("直接获取封装了user信息的result")
     @GetMapping("/getUser")
-    public Result<UserVO> getUser() {
-        return ResultUtil.success( new UserVO(1L,"h","1234"));
+    public Result<UserVO> getUser(@RequestParam @ApiParam(value = "用户唯一标识") Long userId)
+    {
+        return ResultUtil.success( new UserVO(userId,"h","1234"));
     }
 
-    @GetMapping("/getUserList")
-    public Result<List<UserVO>> getUserList() {
+    @PostMapping("/getUserList")
+//    @ApiParam("用户信息")
+    public Result<List<UserVO>> getUserList(@RequestBody  UserVO userVO) {
         List<UserVO> userVOList = new ArrayList<>();
         userVOList.add(new UserVO(1L,null,"1234"));
         return ResultUtil.success(userVOList);
     }
+
+    // @Api 注解用于类上，表示标识这个类是 swagger 的资源。
+    // @ApiOperation 注解用于方法，表示一个 http 请求的操作。
+    // @ApiParam 注解用于参数上，用来标明参数信息。
 }
--- boot-base/src/main/java/ogr/spring/pojo/UserVO.java
+++ boot-base/src/main/java/ogr/spring/pojo/UserVO.java
@@ -1,5 +1,7 @@
 package ogr.spring.pojo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,10 +9,15 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ApiModel("用户类，用来测试")
 public class UserVO {
+    @ApiModelProperty("用户唯一标识")
     private Long id;
+    @ApiModelProperty("用户名")
     private String username;
+    @ApiModelProperty("密码")
     private String password;
+    @ApiModelProperty("爱好")
     private String[] hobby;
 
     public UserVO(Long id, String username, String password) {
@@ -18,4 +25,6 @@ public class UserVO {
         this.username = username;
         this.password = password;
     }
+    //  @ApiModel("用户类，用来测试") 注解于实体类
+    //  @ApiModelProperty("用户名") 注解于类中属性，表示对model属性的说明或者数据操作更改
 }
