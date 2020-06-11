ad054e1dd3476ee433602f1567f1b9dbbc534de7
周锐 <57378028+IloveChina77@users.noreply.github.com>
2020-06-11 19:59:44 +0800

    netty

---
+++ netty/src/main/java/ogr/spring/_1tcp/netty/preface/discard/DiscardServerHandler.java
@@ -0,0 +1,31 @@
+package ogr.spring._1tcp.netty.preface.discard;
+
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+import io.netty.util.ReferenceCountUtil;
+
+
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = (ByteBuf) msg;
+        try{
+            System.err.println(in.toString(CharsetUtil.UTF_8));
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+
+
+}
