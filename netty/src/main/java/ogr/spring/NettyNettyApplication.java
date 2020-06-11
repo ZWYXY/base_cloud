a4afc6cc80b4863d05c9b83b9106f4f7129b9c9f
周锐 <57378028+IloveChina77@users.noreply.github.com>
2020-06-11 20:00:27 +0800

    netty

--- netty/src/main/java/ogr/spring/NettyNettyApplication.java
+++ netty/src/main/java/ogr/spring/NettyNettyApplication.java
@@ -1,13 +1,13 @@
-//package ogr.spring;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//@SpringBootApplication
-//public class NettyNettyApplication {
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(NettyNettyApplication.class, args);
-//    }
-//
-//}
+package ogr.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyNettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyNettyApplication.class, args);
+    }
+
+}
---
+++ netty/src/test/java/ogr/spring/netty/NettyNettyApplicationTests.java
@@ -0,0 +1,13 @@
+package ogr.spring.netty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyNettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
