53de2bfa784ebecfb38c8c8403b590358b2bebd2
周锐 <57378028+IloveChina77@users.noreply.github.com>
2020-07-25 17:53:29 +0800

    修改pom 和 配置文件

---
+++ my-gateway/src/main/java/com/mirco/mygateway/configuration/GatewayConfig.java
@@ -0,0 +1,18 @@
+package com.mirco.mygateway.configuration;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+//public class GatewayConfig {
+//
+//    public RouteLocator customRouterLocator(RouteLocatorBuilder builder) {
+//
+//
+//        return builder.routes()
+//                .route("path_route2", r -> r.path("/"))
+//
+//    }
+//
+//}
---
+++ my-gateway/src/test/java/com/mirco/mygateway/MyGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirco.mygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
