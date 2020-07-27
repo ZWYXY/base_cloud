2f1c8c68e21684f5dec6481c643e28fbf4f9c250
周锐 <57378028+IloveChina77@users.noreply.github.com>
2020-07-27 16:10:26 +0800

    修改点东西

---
+++ remote-service/src/main/java/com/mirco/remoteservice/pojo/po/User.java
@@ -0,0 +1,13 @@
+package com.mirco.remoteservice.pojo.po;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private long userId;
+    private String username;
+    private String password;
+
+
+}
