02f38acb2a7dac681135b09b9c9cad4eead3af8a
周锐 <57378028+IloveChina77@users.noreply.github.com>
2020-07-28 17:08:01 +0800

    新增gateway 配置文件

--- my-gateway/src/main/java/com/mirco/mygateway/configuration/GatewayConfig.java
+++ my-gateway/src/main/java/com/mirco/mygateway/configuration/GatewayConfig.java
@@ -1,19 +1,23 @@
-package com.mirco.mygateway.configuration;
-
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class GatewayConfig {
-
-    @Bean
-    public RouteLocator customRouterLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route("path_route2", r -> r.path("/user/entity")
-                .uri("http://localhost:8101/user/entity"))
-                .build();
-    }
-
-}
+// 就是介绍Spring gateway 如何使用javaBean 配置
+//package com.mirco.mygateway.configuration;
+//
+//import org.springframework.cloud.gateway.route.RouteLocator;
+//import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class GatewayConfig {
+//
+//    @Bean
+//    public RouteLocator customRouterLocator(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route("path_route2", r -> r.path("/user/{userId}")
+//                .uri("http://localhost:8101"))
+//                .route("path_route3", r -> r.path("/user/config")
+//                .uri("http://localhost:8101/user/config"))
+//                .build();
+//
+//    }
+//
+//}
